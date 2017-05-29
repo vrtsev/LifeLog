9cec7280c5a8ee719ec64f1666f465cafaf48f10
vrtsev <v.rtsev@gmail.com>
2017-05-29 11:26:02 +0300

    Ability to view categories of other users

---
+++ app/controllers/publications/users/categories_controller.rb
@@ -0,0 +1,7 @@
+class Publications::Users::CategoriesController < ApplicationController
+  def show
+    @user     = User.find(params[:user_id])
+    @category = @user.publication_categories.find(params[:id])
+    @posts    = @category.publication_posts
+  end
+end
