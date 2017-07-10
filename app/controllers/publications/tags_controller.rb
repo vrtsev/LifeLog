e6ba107f8c974312c390efaf679650e82931afe2
vrtsev <v.rtsev@gmail.com>
2017-07-10 21:03:03 +0300

    Global search of posts by tag

--- app/controllers/publications/tags_controller.rb
+++ app/controllers/publications/tags_controller.rb
@@ -1,7 +1,7 @@
 class Publications::TagsController < ApplicationController
   def show
     @tag = Tag.find_by(name: params[:name])
-    @posts = current_user.publication_posts.joins(:tags)
-                         .where(tags: { name: @tag.name })
+    @posts = Publication::Post.joins(:tags)
+                              .where(tags: { name: @tag.name })
   end
 end
