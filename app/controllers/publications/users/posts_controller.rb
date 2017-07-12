3feed72eb9e93e8ebae787dff47d7c5d5da6ba9b
vrtsev <v.rtsev@gmail.com>
2017-07-12 22:33:54 +0300

    Hiding posts from a user page if they are invisible

--- app/controllers/publications/users/posts_controller.rb
+++ app/controllers/publications/users/posts_controller.rb
@@ -2,12 +2,14 @@ class Publications::Users::PostsController < ApplicationController
   before_action :set_user
 
   def index
-    @posts = @user.publication_posts
+    @posts = @user.publication_posts.visible
     @categories = @user.publication_categories
   end
 
   def show
     @post = @user.publication_posts.find(params[:id])
+
+    redirect_to user_posts_path(@user) unless @post.visible
     @comments = @post.comments.all
   end
 
