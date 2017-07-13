9c3e73acf53810d4774462a094a39882302f7651
vrtsev <v.rtsev@gmail.com>
2017-07-13 21:08:38 +0300

    Hide invisible posts in user categories

--- app/controllers/publications/users/categories_controller.rb
+++ app/controllers/publications/users/categories_controller.rb
@@ -2,6 +2,6 @@ class Publications::Users::CategoriesController < ApplicationController
   def show
     @user     = User.find(params[:user_id])
     @category = @user.publication_categories.find(params[:id])
-    @posts    = @category.publication_posts
+    @posts    = @category.publication_posts.visible
   end
 end
