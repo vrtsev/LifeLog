6fd2521897cccda193b37d06f593f3184c0404e0
vrtsev <v.rtsev@gmail.com>
2017-07-29 16:49:34 +0300

    Temporarily disable all feature spec test

--- spec/features/publications/category_feature_spec.rb
+++ spec/features/publications/category_feature_spec.rb
@@ -1,154 +1,154 @@
-describe 'Publication categories feature', type: :feature do
-  let!(:user) do
-    create :user,
-           name: 'Administrator',
-           email: 'admin@admin.com',
-           password: 'password'
-  end
+# describe 'Publication categories feature', type: :feature do
+#   let!(:user) do
+#     create :user,
+#            name: 'Administrator',
+#            email: 'admin@admin.com',
+#            password: 'password'
+#   end
 
-  let!(:first_category) do
-    create :publication_category,
-           title: 'Trips',
-           description: 'best moments',
-           user: user
-  end
+#   let!(:first_category) do
+#     create :publication_category,
+#            title: 'Trips',
+#            description: 'best moments',
+#            user: user
+#   end
 
-  before do
-    visit new_user_session_path
-    fill_in 'user[email]', with: user.email
-    fill_in 'user[password]', with: 'password'
-    click_button 'Вход'
-  end
+#   before do
+#     visit new_user_session_path
+#     fill_in 'user[email]', with: user.email
+#     fill_in 'user[password]', with: 'password'
+#     click_button 'Вход'
+#   end
 
-  describe '#show' do
-    before do
-      visit posts_path
-      click_link 'Show category'
-    end
+#   describe '#show' do
+#     before do
+#       visit posts_path
+#       click_link 'Show category'
+#     end
 
-    it 'displays category attributes' do
-      expect(page).to have_content first_category.title
-      expect(page).to have_content first_category.description
-    end
-  end
+#     it 'displays category attributes' do
+#       expect(page).to have_content first_category.title
+#       expect(page).to have_content first_category.description
+#     end
+#   end
 
-  describe '#new' do
-    before do
-      visit posts_path
-      click_link 'New Category'
-    end
+#   describe '#new' do
+#     before do
+#       visit posts_path
+#       click_link 'New Category'
+#     end
 
-    it 'displays form for new category' do
-      expect(page).to have_field 'category[title]'
-      expect(page).to have_field 'category[description]'
-      expect(page).to have_button 'Save'
-    end
-  end
+#     it 'displays form for new category' do
+#       expect(page).to have_field 'category[title]'
+#       expect(page).to have_field 'category[description]'
+#       expect(page).to have_button 'Save'
+#     end
+#   end
 
-  describe '#create' do
-    let(:title)       { 'Pets' }
-    let(:description) { 'my home pets' }
+#   describe '#create' do
+#     let(:title)       { 'Pets' }
+#     let(:description) { 'my home pets' }
 
-    context 'valid category' do
-      before do
-        visit posts_path
-        click_link 'New Category'
-        fill_in 'category[title]',       with: title
-        fill_in 'category[description]', with: description
-        click_button 'Save'
-      end
+#     context 'valid category' do
+#       before do
+#         visit posts_path
+#         click_link 'New Category'
+#         fill_in 'category[title]',       with: title
+#         fill_in 'category[description]', with: description
+#         click_button 'Save'
+#       end
 
-      it 'redirect to created post' do
-        expect(page).to have_current_path category_path(Category.last)
-        expect(page).to have_content title
-        expect(page).to have_content description
-      end
-    end
+#       it 'redirect to created post' do
+#         expect(page).to have_current_path category_path(Category.last)
+#         expect(page).to have_content title
+#         expect(page).to have_content description
+#       end
+#     end
 
-    context 'invalid category' do
-      let(:invalid_title)       { '' }
-      let(:invalid_description) { 'a' }
+#     context 'invalid category' do
+#       let(:invalid_title)       { '' }
+#       let(:invalid_description) { 'a' }
 
-      before do
-        visit posts_path
-        click_link 'New Category'
-        fill_in 'category[title]',       with: invalid_title
-        fill_in 'category[description]', with: invalid_description
-        click_button 'Save'
-      end
+#       before do
+#         visit posts_path
+#         click_link 'New Category'
+#         fill_in 'category[title]',       with: invalid_title
+#         fill_in 'category[description]', with: invalid_description
+#         click_button 'Save'
+#       end
 
-      it 'displays form errors' do
-        expect(page).to have_content 'Description is too short'
-        expect(page).to have_content 'Title can\'t be blank'
-      end
-    end
-  end
+#       it 'displays form errors' do
+#         expect(page).to have_content 'Description is too short'
+#         expect(page).to have_content 'Title can\'t be blank'
+#       end
+#     end
+#   end
 
-  describe '#edit' do
-    let(:title_value)       { find_field('category[title]').value }
-    let(:description_value) { find_field('category[description]').value }
+#   describe '#edit' do
+#     let(:title_value)       { find_field('category[title]').value }
+#     let(:description_value) { find_field('category[description]').value }
 
-    before do
-      visit posts_path
-      click_link 'Edit category'
-    end
+#     before do
+#       visit posts_path
+#       click_link 'Edit category'
+#     end
 
-    it 'displays form for existed post' do
-      expect(title_value).to eq first_category.title
-      expect(description_value).to eq first_category.description
-      expect(page).to have_button 'Save'
-    end
-  end
+#     it 'displays form for existed post' do
+#       expect(title_value).to eq first_category.title
+#       expect(description_value).to eq first_category.description
+#       expect(page).to have_button 'Save'
+#     end
+#   end
 
-  describe '#update' do
-    let(:title)       { 'UPDATED' }
-    let(:description) { 'Fresh content' }
+#   describe '#update' do
+#     let(:title)       { 'UPDATED' }
+#     let(:description) { 'Fresh content' }
 
-    context 'valid category' do
-      before do
-        visit posts_path
-        click_link 'Edit category'
-        fill_in 'category[title]',       with: title
-        fill_in 'category[description]', with: description
-        click_button 'Save'
-      end
+#     context 'valid category' do
+#       before do
+#         visit posts_path
+#         click_link 'Edit category'
+#         fill_in 'category[title]',       with: title
+#         fill_in 'category[description]', with: description
+#         click_button 'Save'
+#       end
 
-      it 'redirect to created post' do
-        expect(page).to have_current_path category_path(first_category)
-        expect(page).to have_content title
-        expect(page).to have_content description
-      end
-    end
+#       it 'redirect to created post' do
+#         expect(page).to have_current_path category_path(first_category)
+#         expect(page).to have_content title
+#         expect(page).to have_content description
+#       end
+#     end
 
-    context 'invalid category' do
-      let(:invalid_title)       { '' }
-      let(:invalid_description) { 'a' }
+#     context 'invalid category' do
+#       let(:invalid_title)       { '' }
+#       let(:invalid_description) { 'a' }
 
-      before do
-        visit posts_path
-        click_link 'Edit category'
-        fill_in 'category[title]',       with: invalid_title
-        fill_in 'category[description]', with: invalid_description
-        click_button 'Save'
-      end
+#       before do
+#         visit posts_path
+#         click_link 'Edit category'
+#         fill_in 'category[title]',       with: invalid_title
+#         fill_in 'category[description]', with: invalid_description
+#         click_button 'Save'
+#       end
 
-      it 'displays form errors' do
-        expect(page).to have_content 'Description is too short'
-        expect(page).to have_content 'Title can\'t be blank'
-      end
-    end
-  end
+#       it 'displays form errors' do
+#         expect(page).to have_content 'Description is too short'
+#         expect(page).to have_content 'Title can\'t be blank'
+#       end
+#     end
+#   end
 
-  describe '#destroy' do
-    before do
-      visit posts_path
-      click_link 'Destroy category'
-    end
+#   describe '#destroy' do
+#     before do
+#       visit posts_path
+#       click_link 'Destroy category'
+#     end
 
-    it 'redirect to created post' do
-      expect(page).to have_current_path posts_path
-      expect(page).not_to have_content first_category.title
-      expect(page).not_to have_content first_category.description
-    end
-  end
-end
+#     it 'redirect to created post' do
+#       expect(page).to have_current_path posts_path
+#       expect(page).not_to have_content first_category.title
+#       expect(page).not_to have_content first_category.description
+#     end
+#   end
+# end
--- spec/features/publications/comment_feature_spec.rb
+++ spec/features/publications/comment_feature_spec.rb
@@ -1,138 +1,138 @@
-describe 'Publication comments feature', type: :feature do
-  let!(:first_post) { create :publication_post, user: user }
-  let!(:user) do
-    create :user,
-           name: 'Administrator',
-           email: 'admin@admin.com',
-           password: 'password'
-  end
+# describe 'Publication comments feature', type: :feature do
+#   let!(:first_post) { create :publication_post, user: user }
+#   let!(:user) do
+#     create :user,
+#            name: 'Administrator',
+#            email: 'admin@admin.com',
+#            password: 'password'
+#   end
 
-  let!(:first_comment) do
-    create :publication_comment,
-           content: 'first comment',
-           post: first_post,
-           user: user
-  end
+#   let!(:first_comment) do
+#     create :publication_comment,
+#            content: 'first comment',
+#            post: first_post,
+#            user: user
+#   end
 
-  before do
-    visit new_user_session_path
-    fill_in 'user[email]', with: user.email
-    fill_in 'user[password]', with: 'password'
-    click_button 'Вход'
-  end
+#   before do
+#     visit new_user_session_path
+#     fill_in 'user[email]', with: user.email
+#     fill_in 'user[password]', with: 'password'
+#     click_button 'Вход'
+#   end
 
-  describe 'post page with comments' do
-    before do
-      visit posts_path
-      click_link 'Show post'
-    end
+#   describe 'post page with comments' do
+#     before do
+#       visit posts_path
+#       click_link 'Show post'
+#     end
 
-    it 'displays category attributes' do
-      expect(page).to have_content first_comment.content
-    end
-  end
+#     it 'displays category attributes' do
+#       expect(page).to have_content first_comment.content
+#     end
+#   end
 
-  describe '#new' do
-    before do
-      visit post_path(first_post.id)
-      click_link 'New Comment'
-    end
+#   describe '#new' do
+#     before do
+#       visit post_path(first_post.id)
+#       click_link 'New Comment'
+#     end
 
-    it 'displays form for new category' do
-      expect(page).to have_field 'comment[content]'
-      expect(page).to have_button 'Save'
-    end
-  end
+#     it 'displays form for new category' do
+#       expect(page).to have_field 'comment[content]'
+#       expect(page).to have_button 'Save'
+#     end
+#   end
 
-  describe '#create' do
-    let(:content) { 'its wonderful' }
+#   describe '#create' do
+#     let(:content) { 'its wonderful' }
 
-    context 'valid comment' do
-      before do
-        visit post_path(first_post.id)
-        click_link 'New Comment'
-        fill_in 'comment[content]', with: content
-        click_button 'Save'
-      end
+#     context 'valid comment' do
+#       before do
+#         visit post_path(first_post.id)
+#         click_link 'New Comment'
+#         fill_in 'comment[content]', with: content
+#         click_button 'Save'
+#       end
 
-      it 'redirect to created post' do
-        expect(page).to have_current_path post_path(first_post.id)
-        expect(page).to have_content content
-      end
-    end
+#       it 'redirect to created post' do
+#         expect(page).to have_current_path post_path(first_post.id)
+#         expect(page).to have_content content
+#       end
+#     end
 
-    context 'invalid category' do
-      let(:invalid_content) { 'a' }
+#     context 'invalid category' do
+#       let(:invalid_content) { 'a' }
 
-      before do
-        visit post_path(first_post.id)
-        click_link 'New Comment'
-        fill_in 'comment[content]', with: invalid_content
-        click_button 'Save'
-      end
+#       before do
+#         visit post_path(first_post.id)
+#         click_link 'New Comment'
+#         fill_in 'comment[content]', with: invalid_content
+#         click_button 'Save'
+#       end
 
-      it 'displays form errors' do
-        expect(page).to have_content 'Content is too short'
-      end
-    end
-  end
+#       it 'displays form errors' do
+#         expect(page).to have_content 'Content is too short'
+#       end
+#     end
+#   end
 
-  describe '#edit' do
-    let(:content_value) { find_field('comment[content]').value }
+#   describe '#edit' do
+#     let(:content_value) { find_field('comment[content]').value }
 
-    before do
-      visit post_path(first_post.id)
-      click_link 'Edit comment'
-    end
+#     before do
+#       visit post_path(first_post.id)
+#       click_link 'Edit comment'
+#     end
 
-    it 'displays form for existed post' do
-      expect(content_value).to eq first_comment.content
-      expect(page).to have_button 'Save'
-    end
-  end
+#     it 'displays form for existed post' do
+#       expect(content_value).to eq first_comment.content
+#       expect(page).to have_button 'Save'
+#     end
+#   end
 
-  describe '#update' do
-    let(:content) { 'UPDATED' }
+#   describe '#update' do
+#     let(:content) { 'UPDATED' }
 
-    context 'valid category' do
-      before do
-        visit post_path(first_post.id)
-        click_link 'Edit comment'
-        fill_in 'comment[content]', with: content
-        click_button 'Save'
-      end
+#     context 'valid category' do
+#       before do
+#         visit post_path(first_post.id)
+#         click_link 'Edit comment'
+#         fill_in 'comment[content]', with: content
+#         click_button 'Save'
+#       end
 
-      it 'redirect to created post' do
-        expect(page).to have_current_path post_path(first_post)
-        expect(page).to have_content content
-      end
-    end
+#       it 'redirect to created post' do
+#         expect(page).to have_current_path post_path(first_post)
+#         expect(page).to have_content content
+#       end
+#     end
 
-    context 'invalid category' do
-      let(:invalid_content) { 'a' }
+#     context 'invalid category' do
+#       let(:invalid_content) { 'a' }
 
-      before do
-        visit post_path(first_post)
-        click_link 'Edit comment'
-        fill_in 'comment[content]', with: invalid_content
-        click_button 'Save'
-      end
+#       before do
+#         visit post_path(first_post)
+#         click_link 'Edit comment'
+#         fill_in 'comment[content]', with: invalid_content
+#         click_button 'Save'
+#       end
 
-      it 'displays form errors' do
-        expect(page).to have_content 'Content is too short'
-      end
-    end
-  end
+#       it 'displays form errors' do
+#         expect(page).to have_content 'Content is too short'
+#       end
+#     end
+#   end
 
-  describe '#destroy' do
-    before do
-      visit post_path(first_post)
-      click_link 'Destroy comment'
-    end
+#   describe '#destroy' do
+#     before do
+#       visit post_path(first_post)
+#       click_link 'Destroy comment'
+#     end
 
-    it 'redirect to created post' do
-      expect(page).to have_current_path post_path(first_post)
-      expect(page).not_to have_content first_comment.content
-    end
-  end
-end
+#     it 'redirect to created post' do
+#       expect(page).to have_current_path post_path(first_post)
+#       expect(page).not_to have_content first_comment.content
+#     end
+#   end
+# end
