8741edc12e899fb1f8ed17bf0974ee9cec0cf5cd
vrtsev <v.rtsev@gmail.com>
2017-07-30 17:07:45 +0300

    Frontend for categories of users

--- app/controllers/publications/users_controller.rb
+++ app/controllers/publications/users_controller.rb
@@ -1,17 +1,24 @@
 class Publications::UsersController < PublicationsController
   layout 'user_publications'
 
-  def index
-    @users = User.all
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 
-  def show
-    user = User.find(params[:id])
 
-    if user == current_user
-      redirect_to posts_path
-    else
-      redirect_to user_posts_path(user)
-    end
-  end
+  # def index
+  #   @users = User.all
+  # end
+
+  # def show
+  #   user = User.find(params[:id])
+
+  #   if user == current_user
+  #     redirect_to posts_path
+  #   else
+  #     redirect_to user_posts_path(user)
+  #   end
+  # end
 end
