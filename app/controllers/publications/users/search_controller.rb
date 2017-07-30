fc8153ef3b1526926977793c4617818d593409d4
vrtsev <v.rtsev@gmail.com>
2017-07-30 18:00:30 +0300

    Frontend for user search and some fixes for tags controller

--- app/controllers/publications/users/search_controller.rb
+++ app/controllers/publications/users/search_controller.rb
@@ -1,5 +1,5 @@
-class Publications::Users::SearchController < ApplicationController
-  before_action :set_user
+class Publications::Users::SearchController < Publications::UsersController
+  before_action :set_search_values
 
   def index
     @tags     = find_tags
@@ -9,25 +9,34 @@ class Publications::Users::SearchController < ApplicationController
 
   private
 
-  def set_user
-    @user = User.find(params[:user_id])
-  end
-
   def find_tags
-    Publications::TagsSearchQuery.new(
-      @user, params[:query], params[:tag]
+    scope = Tag.joins(:taggings).joins(:posts)
+               .where(posts: { user: @user, type: 'Publication::Post' })
+               .distinct
+
+    TagsSearchQuery.new(
+      scope, params[:query], params[:tag]
     ).results
   end
 
   def find_posts
-    Publications::PostsSearchQuery.new(
-      @user, params[:query], params[:post]
+    PostsSearchQuery.new(
+      @user.publication_posts, params[:query], params[:post]
     ).results
   end
 
   def find_comments
-    Publications::CommentsSearchQuery.new(
-      @user, params[:query], params[:comment]
+    CommentsSearchQuery.new(
+      @user.publication_comments, params[:query], params[:comment]
     ).results
+  end
+
+  def set_search_values
+    return unless params[:post].present?
+
+    @filter_pinned       = params[:post][:pinned]
+    @filter_supplemented = params[:post][:supplemented]
+    @filter_visible      = params[:post][:visible]
+    @filter_commentable  = params[:post][:commentable]
   end
 end
