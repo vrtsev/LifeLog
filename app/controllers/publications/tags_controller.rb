d50ec8a1505706cdbaa2d1ec6c919d3bc108d594
vrtsev <v.rtsev@gmail.com>
2017-07-30 18:00:30 +0300

    Frontend for user search and some fixes for tags controller

--- app/controllers/publications/tags_controller.rb
+++ app/controllers/publications/tags_controller.rb
@@ -1,6 +1,8 @@
 class Publications::TagsController < PublicationsController
   def show
     @tag = Tag.find_by(name: params[:name])
+
+    return unless @tag.present?
     @posts = Publication::Post.joins(:tags)
                               .where(tags: { name: @tag.name })
   end
