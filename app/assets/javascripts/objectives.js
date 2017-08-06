2a8e83164bedb2c7f1b887b3c24721b75e230d9d
vrtsev <v.rtsev@gmail.com>
2017-08-06 16:48:58 +0300

    Goals has beautiful frontend now

--- app/assets/javascripts/objectives.js
+++ app/assets/javascripts/objectives.js
@@ -1,1 +1,36 @@
 //= require shared
+//= require moment 
+//= require fullcalendar
+
+function eventCalendar() {
+  return $('#calendar').fullCalendar({ events: '/objectives/goals.json' });
+};
+
+function clearCalendar() {
+  $('#calendar').fullCalendar('delete'); // In case delete doesn't work.
+  $('#calendar').html('');
+};
+
+$(document).on('turbolinks:load', eventCalendar);
+$(document).on('turbolinks:before-cache', clearCalendar)
+
+
+$(document).on('turbolinks:load', function() {
+  // Hide/Show goal block
+  $('#goal-hide-btn').on('click', function(e) {
+    var buttonIcon = $('#hide-btn-icon')
+    var iconUp     = buttonIcon.hasClass('fa-chevron-up')
+    var iconDown   = buttonIcon.hasClass('fa-chevron-down')
+
+    e.preventDefault();
+    $('#goal-description').slideToggle('fast');
+    
+    if (iconUp) {
+      buttonIcon.removeClass();
+      buttonIcon.addClass('fa fa-chevron-down text-primary');
+    } else if (iconDown) { 
+      buttonIcon.removeClass();
+      buttonIcon.addClass('fa fa-chevron-up');
+    }
+  });
+});
--- db/migrate/20170715191630_create_goals.rb
+++ db/migrate/20170715191630_create_goals.rb
@@ -1,12 +1,12 @@
 class CreateGoals < ActiveRecord::Migration[5.0]
   def change
     create_table :goals do |t|
-      t.string :title
-      t.text :description
+      t.string  :title
+      t.text    :description
       t.integer :status, default: 0
       t.integer :progress, default: 0
-      t.datetime :start_date
-      t.datetime :end_date
+      t.date    :start_date
+      t.date    :end_date
       t.integer :parent_id
       t.integer :category_id
       t.integer :user_id, null: false, index: true
