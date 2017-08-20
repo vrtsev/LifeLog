102936880c35d8da76dbc01ee34c36d6087418e1
vrtsev <v.rtsev@gmail.com>
2017-08-20 17:40:58 +0300

    Integrate flash messages to layouts

--- app/assets/javascripts/shared.js
+++ app/assets/javascripts/shared.js
@@ -32,4 +32,11 @@ $(document).on('turbolinks:load', function() {
     $('#new-comment-form').toggleClass('hidden');
     $('.new-comment-input').focus();
   });
+
+  var messageBox = $('.flash-message-box')
+  if (messageBox.length == 1) {
+    setTimeout(function() {
+      messageBox.fadeOut('fast');
+    }, 5000);
+  }
 })
