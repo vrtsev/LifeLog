f654b8a5003f3428a4df213557caaee96665fb6d
vrtsev <v.rtsev@gmail.com>
2017-08-20 18:42:39 +0300

    Globally add 'no content' placeholders

--- app/controllers/diary/tags_controller.rb
+++ app/controllers/diary/tags_controller.rb
@@ -1,8 +1,11 @@
 class Diary::TagsController < DiaryController
   def show
     @tag = Tag.find_by(name: params[:name])
-    @posts = current_user.diary_posts.joins(:tags)
-                         .where(tags: { name: @tag.name })
-                         .paginate(page: params[:page], per_page: 10)
+
+    if @tag.present?
+      @posts = current_user.diary_posts.joins(:tags)
+                          .where(tags: { name: @tag.name })
+                          .paginate(page: params[:page], per_page: 10)
+    end
   end
 end
