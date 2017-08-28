32e1f8b8fa4ca71c5700d5fdf9bb846351a7f601
vrtsev <v.rtsev@gmail.com>
2017-08-28 21:15:53 +0300

    Cleanup admin controllers

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,12 +1,19 @@
 class AdminController < ApplicationController
   layout 'admin'
-  before_action :set_variables
+  before_action :check_admin, :set_layout_variables
   skip_before_action :track_activity
 
   private
 
-  def set_variables
-    @users = User.all
+  def check_admin
+    return if current_user.admin?
+
+    flash[:error] = 'You don\'t have permissions to visit this page'
+    redirect_to root_path
+  end
+
+  def set_layout_variables
+    @users_count = User.all.count
     @visits_per_day = Visit.where("DATE(started_at) = ?", DateTime.now).count
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :track_activity
+  before_action :authenticate_user!, :track_activity
 
   private
 
